f7e97eeaffdaeb914429459daede372855d2c5b1
bninamango.personal <bninamango.personal@gmail.com>
2022-11-22 21:25:30 -0500

    ..

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace bninamango
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fibonacci : " + Fibonacci(3));
+        }
+
+        private static int Fibonacci(int n)
+        {
+            if (n == 0) return 0;
+
+            if (n == 1) return 1;
+
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
